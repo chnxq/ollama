1d2b9fca95747820d25a8594f25595aac1793dae
Bruce MacDonald <brucewmacdonald@gmail.com>
2025-04-09 13:47:20 -0700

    Update process_image_test.go

--- model/models/qwen25vl/process_image_test.go
+++ model/models/qwen25vl/process_image_test.go
@@ -38,52 +38,10 @@ func TestSmartResize(t *testing.T) {
 
 	for _, c := range cases {
 		b := c.TestImage.Bounds().Max
-		actual := processor.smartResize(b)
+		x, y := processor.SmartResize(b.X, b.Y)
+		actual := image.Point{x, y}
 		if actual != c.Expected {
 			t.Errorf("expected: %v, actual: %v", c.Expected, actual)
-		}
-	}
-}
-
-func TestProcessImage(t *testing.T) {
-	type processImageCase struct {
-		TestImage   image.Image
-		ExpectedLen int
-	}
-
-	// Create an image processor with default values
-	processor := ImageProcessor{
-		imageSize:   560, // Example value
-		numChannels: 3,
-		factor:      28,
-		minPixels:   56 * 56,
-		maxPixels:   14 * 14 * 4 * 1280,
-	}
-
-	cases := []processImageCase{
-		{
-			TestImage:   image.NewRGBA(image.Rect(0, 0, 256, 256)),
-			ExpectedLen: 252 * 252 * 3,
-		},
-		{
-			TestImage:   image.NewRGBA(image.Rect(0, 0, 2000, 2000)),
-			ExpectedLen: 980 * 980 * 3,
-		},
-	}
-
-	for _, c := range cases {
-		imgData, err := processor.ProcessImage(c.TestImage)
-		if err != nil {
-			t.Fatalf("error processing: %q", err)
-		}
-
-		switch len(imgData) {
-		case 0:
-			t.Errorf("no image data returned")
-		case c.ExpectedLen:
-			// ok
-		default:
-			t.Errorf("unexpected image data length: %d, expected: %d", len(imgData), c.ExpectedLen)
 		}
 	}
 }
